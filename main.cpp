8e32d3c4bd963d0f2fa9b02f712b55d839b0e318
RyuJyashinn <k024g1044@g.neec.ac.jp>
2025-04-18 15:05:04 +0800

    4.18

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 ﻿#include<stdio.h>
 #include<Windows.h>
+
+template<typename Type>
+Type typeCompare(Type a, Type b) {
+	if (a >= b) {
+		return a;
+	} else if (a < b) {
+		return b;
+	}
+}
+
+template<>
+char typeCompare<char>(char a, char b) {
+	printf("error");
+}
+
+
 int main() {
-	SetConsoleOutputCP(65001);
-	char str[] = "夜露死苦";
-	printf("%s",str);
+	int a = 12;
+	int b = 15;
+	printf("%d %d max:%d", a, b, typeCompare<int>(a, b));
+	float c = 12.0f;
+	float d = 15.0f;
+	printf("%f %f max:%d", c, d, typeCompare<float>(c, d));
+	double e = 12.00f;
+	double f = 15.00f;
+	printf("%d %d max:%d", e, f, typeCompare<double>(e, f));
+
 	return 0;
 }
