64daf715e3d09b91013ea355c6fe5286fdfef67a
RyuJyashinn <k024g1044@g.neec.ac.jp>
2025-04-25 15:31:35 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 #include<stdio.h>
-
-int regularPayment(int time) {
+#include <string.h>
+int regularSalary(int time) {
 	int result = time * 1072;
 	return result;
 }
 
-int changePayment(int time) {
-
-
+int totalSalary(int time, int lastSalary, int total) {
+	if (time == 1) {
+		return total + lastSalary;
+	} else {
+		int currentSalary = 2 * lastSalary - 50;
+		return totalSalary(time - 1, currentSalary, total + lastSalary);
+	}
 }
 
-int main(void) {
 
+int main() {
+	int tempTime = -1;
+	printf("勤務時間を入力してください:");
+	scanf_s("%d", &tempTime);
+	int regularMoney = regularSalary(tempTime);
+	int changingMoney= totalSalary(tempTime,100,0);
+	char result[20];
+	if (changingMoney >= regularMoney) {
+		strcpy_s(result, "再帰的な賃金体系");
+	} else {
+		strcpy_s(result, "一般的な賃金体系");
 
-
+	}
+	printf("一般的な賃金体系は:%d円\n再帰的な賃金体系は:%d円\nより多い方は:%s", regularMoney, changingMoney,result);
 	return 0;
 }
