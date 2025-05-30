d562848d3f2d4cae2802753408f297ce840377e0
RyuJyashinn <k024g1044@g.neec.ac.jp>
2025-05-30 15:53:16 +0800

    111

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
 ﻿#include <stdio.h>
 #include<iostream>
-
-class Vehicle {
-public:
-    virtual void engineType() const = 0;
-};
-
-class Ev : public Vehicle {
-public:
-    void engineType() const override {
-        printf("Electric ButterEngine\n");
-    }
-};
-
-class Car :public Vehicle {
-public:
-    void engineType() const override {
-        printf("Gasoline Engine\n");
-    }
-};
+#include<list>
+using namespace std;
 
 int main() {
-    Vehicle* vehicle1 = new Ev();
-    Vehicle* vehicle2 = new Car();
+	list<const char*> eki_list{
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Sin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Sinagawa", "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
 
-    vehicle1->engineType();
-    vehicle2->engineType();
+	printf("1970\n");
+	list<const char*>::iterator i;
+	for (i = eki_list.begin(); i != eki_list.end(); ++i) {
+		std::cout << *i << endl;
+	}
 
-    delete vehicle1;
-    delete vehicle2;
+	printf("2019\n");
+	list<const char*>::iterator j;
+	for (j = eki_list.begin(); j != eki_list.end(); ++j) {
+		if (*j == "Tabata") {
+			j = eki_list.insert(j, "Nishi-Nippori"); 
+			std::cout << *j << endl; 
+			++j; 
+		}
+		std::cout << *j << endl; 
+	}
+
+	printf("2020\n");
+	list<const char*>::iterator k;
+	for (k = eki_list.begin(); k != eki_list.end(); ++k) {
+		if (*k == "Tamachi") {
+			k = eki_list.insert(k, "Takanawa Gateway");
+			std::cout << *k << endl;
+			++k;
+		}
+		std::cout << *k << endl;
+	}
 	return 0;
 }
